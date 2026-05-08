7a2c9526b5709cf8b6002c63b586fe8ff9bf4c9d
Mahmoud-Mshrf <104544236+Mahmoud-Mshrf@users.noreply.github.com>
2026-05-09 00:39:23 +0300

    functional programming and higher order functions

---
+++ HigherOrderFunctions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HigherOrderFunctions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
