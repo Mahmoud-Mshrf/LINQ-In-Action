639caedc6e06308d9ab54cb20e7c20f7e8dce2fc
Mahmoud-Mshrf <104544236+Mahmoud-Mshrf@users.noreply.github.com>
2026-05-08 23:33:35 +0300

    Pure vs Impure function and statement vs expression

---
+++ PureVsImpureFunctions/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace PureVsImpureFunctions
+{
+    // Pure function : the function is pure if:
+    // - always produces the same output for the same input 
+    // - has no side effects ( does not change external state)
+
+    internal class Program
+    {
+        static List<int> Numbers = new List<int> {1,2,3,4,5,6,7,8,9};// global variable
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Before mutation");
+            Print(Numbers);
+            Console.WriteLine("After mutation");
+            AddInteger1(3);
+            Print(Numbers);
+            Console.WriteLine("the new list returned from the pure function");
+            var newNumbers = GetNumbers(Numbers, 10);
+            Print(newNumbers);
+        }
+        static void Print<T>(IEnumerable<T> values) // pure function
+        {
+            foreach (T value in values)
+            {
+                Console.Write(value);
+            }
+            Console.WriteLine();
+        }
+        // examples of impure functions :
+        static void AddInteger1(int number)
+        {
+            Numbers.Add(number); // this is impure because it mutate(change) global variable it add number to it
+        }
+        static void AddInteger2(ref int number)
+        {
+            number++;
+            Numbers.Add(number); // this is impure because it mutate(change) global variable it add number to it
+                                 // and it mutate parameter it change its value , the number increased by 1
+        }
+        static void AddInteger3()
+        {
+            Numbers.Add(new Random().Next());
+            // this is impure becuase it interact with outside world 
+            // and it mutate the global variable 
+        }
+        static List<int> GetNumbers(List<int> numbers , int num)
+        {
+            var list = new List<int>(numbers);
+            list.Add(num);
+            return list;
+            // this is pure because it doesn't has side effects it create a new list and add a number to it so it has same output for same input and doesn't mutate anything
+        }
+    }
+}
---
+++ StatementVsExpression/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace StatementVsExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Statement: is an instruction that performs an action 
+            // examples 
+            // declaration statement:
+            int x;
+            // assignment statement:
+            x = 0;
+            // declare and initialization statement
+            int y = 0;
+            // for , foreach , do-while , while , if , if-else , switch statement 
+            // all of these performs an action then it's statement
+            // --------------------------------------
+
+            // Expression: is a code that produces a value
+            // x + y;      // expression returns sum of two numbers
+            // method invokation // produce method return value 
+            // object creation 
+            // --------------------------------------
+
+            // switch statemnt Vs switch expression :
+            string name;
+            x = 0;
+            // switch statement
+            switch (x)
+            {
+                case 1:
+                    name = "a";
+                    break;
+                case 2:
+                    name = "b";
+                    break;
+                case 3:
+                    name = "c";
+                    break;
+                default:
+                    name = "z";
+                    break;
+            }
+            // switch expression
+            name = x switch
+            {
+                1 => "a",
+                2 => "b",
+                3 => "c",
+                _ => "z",
+            };
+
+            // --------------------------------------
+            /*
+             🔹 Statements Categories
+                Examples:
+
+                  if
+                  for
+                  foreach
+                  while
+                  return
+                  break
+                  declaration statements
+
+             🔹 Expression Categories
+                Examples:
+                
+                  literals
+                  arithmetic
+                  method calls
+                  lambda expressions
+                  object creation
+                  comparisons
+             */
+
+        }
+    }
+}
