05d3d772ccdeff745c1e38f81a7f4cb99e3bcf23
Mahmoud-Mshrf <104544236+Mahmoud-Mshrf@users.noreply.github.com>
2026-05-10 14:19:24 +0300

    Grouping Data

---
+++ Grouping/Program.cs
@@ -0,0 +1,45 @@
+﻿using _08_Shared;
+
+namespace Grouping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var emps = Repository.LoadEmployees();
+            GroupByMethod(emps);
+            GroupByQuery(emps);
+            ToLookUp(emps);
+        }
+
+        private static void ToLookUp(IEnumerable<Employee> emps)
+        {
+            var groups = emps.ToLookup(x => x.Department); // Immediate execution
+            foreach (var group in groups)
+            {
+                group.Print($"Employees in {group.Key} Department");
+            }
+        }
+
+        private static void GroupByQuery(IEnumerable<Employee> emps)
+        {
+            // deffered execution
+            var groups = from emp in emps
+                         group emp by emp.Department;
+            foreach (var group in groups)
+            {
+                group.Print($"Employees in {group.Key} Department");
+            }
+        }
+
+        private static void GroupByMethod(IEnumerable<Employee> emps)
+        {
+            // deffered execution
+            var groups = emps.GroupBy(x => x.Department);
+            foreach (var group in groups)
+            {
+                group.Print($"Employees in {group.Key} Department");
+            }
+        }
+    }
+}
--- Quantifiers/Program.cs
+++ Quantifiers/Program.cs
@@ -11,6 +11,14 @@ namespace Quantifiers
             bool result1 = emps.Any(x => x.Salary > 20000);// true if there is any employee his salary more than 20000
             bool result2 = emps.Any(x => x.Skills.Count == 1);// true if there is any employee has only one skill
             bool result3 = emps.Any(x => x.Name.Contains("AA"));// true if there is any employee that his name contains 'AA'
+            // any query syntax
+            // Any with query syntax return the employees there hava C# in their skills
+            var result00 = from employee in emps
+                          where employee.Skills.Any(s => s == "C#")
+                          select employee;
+            var result01 = from employee in emps
+                          where employee.Skills.Any(s => s.Length >= 3)// return employees with skills have more than 3 characters
+                          select employee;
             // All
             bool result4 = emps.All(x => x.Salary > 1000);// true if all employees have salary more than 1000
             bool result5 = emps.All(x => x.Skills.Count >= 1);// true if all employees have more than one skill
@@ -25,6 +33,7 @@ namespace Quantifiers
             // it will return false although there are an employee have the same email but it return false,
             // because it a reference type must ovveride equality methods to compare valuse not references
             // if we ovveride it will return true
+            Console.WriteLine(result10);// true because we override equality
         }
     }
 }
