9fa40371702666f3c6b62f1b6ebd01fc3cf65b59
Mahmoud-Mshrf <104544236+Mahmoud-Mshrf@users.noreply.github.com>
2026-05-12 21:32:43 +0300

    Linq Anatomy

---
+++ LinqAnatomy/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqAnatomy
+{
+    public static class Extensions
+    {
+        public static void PrintDeck(this IEnumerable<Card> cards, string title)
+        {
+            Console.WriteLine($"\n\n\n###### {title} ######");
+            foreach (Card card in cards)
+            {
+                Console.WriteLine(card.Name);
+            }
+        }
+    }
+}
